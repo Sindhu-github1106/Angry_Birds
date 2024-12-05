b31eaee2592746b026f85739466efb8a2f82682f
Venkata Sri Sai Sindhu <106739392+Sindhu-github1106@users.noreply.github.com>
2024-12-06 00:35:29 +0530

    Update ThemeChoose.java

--- core/src/main/java/io/github/some_example_name/ThemeChoose.java
+++ core/src/main/java/io/github/some_example_name/ThemeChoose.java
@@ -11,104 +11,87 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class ThemeChoose implements Screen {
+public class HomeScreen implements Screen {
     private final Game game;
     private final SpriteBatch batch;
     private Texture backgroundImage;
-    private Texture night_themeImage;
-    private Texture day_themeImage;
-    private Texture halloween_themeImage;
-    private Rectangle themenightRectangle;
-    private Rectangle themedayRectangle;
-    private Rectangle themehalloweenRectangle;
-    private String selectedTheme;
-    private Texture homeButtonImage;
-    private Circle speakerCircle;
-    private Rectangle homeButtonRectangle;
-    private Texture backgroundMusicImage;
+    private Texture musicIcon;
+    private Texture startButtonImage;
+    private Texture resumeButtonImage;
+    private Circle musicIconCircle;
+    private Rectangle startNewGameRectangle;
+    private Rectangle resumeGameRectangle;
     private Music backgroundMusic;
-    private boolean resumeMode;
+    private Texture[] storySlides;  // array of textures containing story slide images
+    private int currentSlide = -1;// current slide index
+    public boolean resume;
 
-    public ThemeChoose(Game game, boolean resumeMode) {
+    public HomeScreen(Game game) {
         this.game = game;
         this.batch = ((Main) game).getBatch();
-        this.resumeMode = resumeMode;
     }
 
     @Override
     public void show() {
-        backgroundImage = new Texture("theme_background.png");
-        night_themeImage = new Texture("night_theme.png");
-        day_themeImage = new Texture("day_theme.png");
-        halloween_themeImage =new Texture("spooky_theme.png");
-        backgroundMusicImage = new Texture("download2.png"); // 50*50
-        homeButtonImage = new Texture("homebutton.png");
 
-        speakerCircle = new Circle(120, 455, 22.5f);
-        homeButtonRectangle = new Rectangle(18.2F, 418.8F, 70, 70);
-        themenightRectangle = new Rectangle(50, 15.6F, 216, 383);
-        themedayRectangle = new Rectangle(292, 15.6F, 216, 383);
-        themehalloweenRectangle=new Rectangle(533.6F, 15.6F, 216, 383);
+        backgroundImage = new Texture("bgm.png");
+        musicIcon = new Texture("download2.png");
+        startButtonImage = new Texture("startt.png");
+        resumeButtonImage = new Texture("resumee.png");
+
+        storySlides = new Texture[]{new Texture("story_slide_1.png"), new Texture("story_slide_2.png"), new Texture("story_slide_3.png"), new Texture("story_slide_4.png")};
+        musicIconCircle = new Circle(55, 452, 22.5f);
+        startNewGameRectangle = new Rectangle(185, 65, 180, 50);
+        resumeGameRectangle = new Rectangle(435, 65, 180, 50);
+
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("bgm.mp3"));
-        backgroundMusic.setLooping(true);
+        backgroundMusic.setLooping(true); // music loop
+
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                float adjustedY = 500 - screenY;
-                if (speakerCircle.contains(screenX, adjustedY)) {
+                float newy = 500 - screenY;
+
+                if (musicIconCircle.contains(screenX, newy)) {
                     ((Main) game).getMusicManager().toggleMusic();
                     return true;
                 }
-                if (homeButtonRectangle.contains(screenX, adjustedY)) {
-                    game.setScreen(new HomeScreen(game));
-
-                    return true;
+                if (currentSlide == -1) {  // home screen
+                    if (startNewGameRectangle.contains(screenX, newy)) {
+                        currentSlide = 0;// start the story slides
+                        resume=false;
+                    }
+                    if (resumeGameRectangle.contains(screenX, newy)) {
+                        resume=true;
+                        game.setScreen(new ThemeChoose(game,resume));
+                    }
+                } else {  //in the story slides
+                    currentSlide++;//next slide
+                    if (currentSlide >= storySlides.length) {
+                        game.setScreen(new ThemeChoose(game, resume));
+                        return true;
+                    }
                 }
-
-                if (themenightRectangle.contains(screenX, adjustedY)) {
-                    selectedTheme="night.png";
-                    if (resumeMode) {
-
-                        game.setScreen(new LevelMap_Resume(game, "night_resume.png"));
-                    } else {
-                        game.setScreen(new LevelMap_Start(game, selectedTheme));
-                    }
-                    return true;
-                } else if (themedayRectangle.contains(screenX, adjustedY)) {
-                    selectedTheme = "day.png";
-                    if (resumeMode) {
-                        game.setScreen(new LevelMap_Resume(game, "day_resume.png"));
-                    } else {
-                        game.setScreen(new LevelMap_Start(game, selectedTheme));
-                    }
-                    return true;
-                } else if (themehalloweenRectangle.contains(screenX, adjustedY)) {
-                    selectedTheme = "spooky.png";
-                    if (resumeMode) {
-                        game.setScreen(new LevelMap_Resume(game,"spooky_resume.png"));
-                    } else {
-                        game.setScreen(new LevelMap_Start(game, selectedTheme));
-                    }
-                    return true;
-                }
-
-                return false;
+                return true;
             }
         });
     }
+
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(1, 1, 1, 1);
 
         batch.begin();
-        batch.draw(backgroundImage, 0, 0, 800, 500);
-        batch.draw(night_themeImage, themenightRectangle.x,themenightRectangle.y,themenightRectangle.width,themenightRectangle.height);
-        batch.draw(day_themeImage, themedayRectangle.x,themedayRectangle.y,themedayRectangle.width,themedayRectangle.height);
-        batch.draw(halloween_themeImage, themehalloweenRectangle.x,themehalloweenRectangle.y,themehalloweenRectangle.width,themehalloweenRectangle.height);
-        batch.draw(homeButtonImage,homeButtonRectangle.x, homeButtonRectangle.y, homeButtonRectangle.width, homeButtonRectangle.height);
-        batch.draw(backgroundMusicImage, speakerCircle.x - 25, speakerCircle.y - 25, 50, 50);
+        if (currentSlide == -1) {
 
+            batch.draw(backgroundImage, 0, 0, 800, 500);
+            batch.draw(musicIcon, 30, 426, 50, 50);
+            batch.draw(startButtonImage, startNewGameRectangle.x, startNewGameRectangle.y, startNewGameRectangle.width, startNewGameRectangle.height);
+            batch.draw(resumeButtonImage, resumeGameRectangle.x, resumeGameRectangle.y, resumeGameRectangle.width, resumeGameRectangle.height);
+        } else {
+            batch.draw(storySlides[currentSlide], 0, 0, 800, 500);
+        }
         batch.end();
     }
 
@@ -127,11 +110,12 @@ public class ThemeChoose implements Screen {
     @Override
     public void dispose() {
         backgroundImage.dispose();
-        night_themeImage.dispose();
-        day_themeImage.dispose();
-        halloween_themeImage.dispose();
+        musicIcon.dispose();
+        startButtonImage.dispose();
+        resumeButtonImage.dispose();
         backgroundMusic.dispose();
-        backgroundMusicImage.dispose();
-        homeButtonImage.dispose();
+        for (Texture slide : storySlides) {
+            slide.dispose();
+        }
     }
 }
